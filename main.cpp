e526620306f7c12acdb7d0dc4fc844fe4cbeb458
Sarp Akın <sarp4.akin@gmail.com>
2020-09-09 22:23:12 +0300

    Added OlcpixelGameEgine

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-#include <vector>
-
 
 int main()
 {
-    std::vector<int> number = {1,5,1,34,18,29};
-    for(int i : number)
-    {
-        std::cout << i << std::endl;
-    }
+
 }

d372b1d51fa28d8a14b1a3626d4571528b969a29
SarpAkin <71023503+SarpAkin@users.noreply.github.com>
2020-09-21 19:23:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "src\Game.h"
 
-//for mingw: g++ -o game.exe  main.cpp src\*.cpp dependencies\*cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
-
 int main()
 {
     #ifdef _Debug
